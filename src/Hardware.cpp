bd99c2bce670169e2d6d7f05c095556391181392
rjmichaelii <rjmichaelii@alaska.edu>
2023-08-08 11:35:41 -0800

    Created Hardware.cpp

--- src/Hardware.cpp
+++ src/Hardware.cpp
@@ -2,4 +2,22 @@
 
 Hardware::Hardware()
 {
+    // Indivudal Motors
+
+    // Left
+    front_left_drivetrain_motor.setBrake(vex::brakeType::brake);
+    middle_left_drivetrain_motor.setBrake(vex::brakeType::brake);
+    back_left_drivetrain_motor.setBrake(vex::brakeType::brake);
+
+    // Right
+    front_right_drivetrain_motor.setBrake(vex::brakeType::brake);
+    middle_left_drivetrain_motor.setBrake(vex::brakeType::brake);
+    back_right_drivetrain_motor.setBrake(vex::brakeType::brake);
+
+    // Motor Groups
+    left_drivetrain_motors.setBrake(vex::brakeType::brake);
+    right_drivetrain_motors.setBrake(vex::brakeType::brake);
+
+    // Drivetrain
+    drivetrain.setStopping(vex::brakeType::brake);
 }
