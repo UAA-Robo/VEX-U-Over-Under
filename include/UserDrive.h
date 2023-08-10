adc8b364238d7829f7520ad6a6d4a96ce48d5096
rjmichaelii <rjmichaelii@alaska.edu>
2023-08-09 17:56:57 -0800

    Created UserDrive.h & UserDrive.cpp

--- include/UserDrive.h
+++ include/UserDrive.h
@@ -1,9 +1,14 @@
 #pragma once
 #include "Drive.h"
 
-class UserDrive : public Drive{
+class UserDrive : public Drive
+{
 public:
-    UserDrive(Hardware* hardware, RobotConfig* robotConfig, Telemetry* telemetry);
+    UserDrive(Hardware *hardware, RobotConfig *robotConfig, Telemetry *telemetry);
     void drive();
 
+private:
+    /// @brief  Controls drivetrain based on controller joysticks. Up/down on the left joystick is forward/backward.
+    //          Left/right on the right joystick is turning.
+    void drivetrain_controls();
 };
--- src/UserDrive.cpp
+++ src/UserDrive.cpp
@@ -1,8 +1,30 @@
 #include "UserDrive.h"
 
-UserDrive::UserDrive(Hardware* hardware, RobotConfig* robotConfig, Telemetry* telemetry): Drive(hardware, robotConfig, telemetry) 
-{}
+UserDrive::UserDrive(Hardware *hardware, RobotConfig *robotConfig, Telemetry *telemetry) : Drive(hardware, robotConfig, telemetry)
+{
+}
 
 void UserDrive::drive()
-{   
+{
+    drivetrain_controls();
+}
+
+void UserDrive::drivetrain_controls()
+{
+    const int DEADZONE = 2;
+
+    double forward_backward = (double)hw->controller.Axis3.position(vex::percentUnits::pct);
+    double turning = (double)hw->controller.Axis1.position(vex::percentUnits::pct);
+
+    if (std::abs(forward_backward) < DEADZONE)
+    {
+        forward_backward = 0;
+    }
+
+    if (std::abs(turning) < DEADZONE)
+    {
+        turning = 0;
+    }
+
+    move_drivetrain({forward_backward, turning});
 }
