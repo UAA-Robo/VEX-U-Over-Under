75c3ae486b811ff81c6655e96b9ca97d3b6ef164
Mya Schroder <80082701+schromya@users.noreply.github.com>
2023-09-05 09:18:26 -0800

    Initial ControllerWrapper framework finished but does NOT work bc tButtonType is private in vex::controller and Controller::button_type can not be converted to that

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -1,5 +1,27 @@
 #include "Controller.h"
-Controller::Controller(const vex::controllerType controller_type) : vex::controller(controller_type){
+
+Controller::Controller(vex::controllerType controller_type){
+    controller(controller_type);
 
 }
+
+Controller::Button::Button(Controller::button_type id, Controller *parent ) {
+    controller::button(id, parent);
+} 
+
+Controller::Axis::Axis(const vex::axisType id, Controller *parent) {
+    controller::axis(id, parent);
+}
+
+void Controller::record_macro(bool IS_MACRO_RECORDING) {}
+
+void Controller::run_macro(bool IS_MACRO_RUNNING) {}
+
+bool Controller::is_recording() {}
+
+bool Controller::is_running() {}
+
+bool Controller::Button::pressing() {}
+
+int Controller::Axis::value() {}
 
