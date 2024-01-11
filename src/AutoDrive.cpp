fa7d695f00cbfff5b313ca91cb0546f515479890
Mya Schroder <80082701+schromya@users.noreply.github.com>
2024-01-10 15:41:44 -0900

    Tried so hard

--- src/AutoDrive.cpp
+++ src/AutoDrive.cpp
@@ -9,7 +9,7 @@ AutoDrive::AutoDrive(Hardware *hardware, RobotConfig *robotConfig, Telemetry *te
 
 void AutoDrive::drive() {
     // Set braking
-    std::cout << "HHERE" << std::endl;
+    std::cout << "START --------------" << std::endl;
     hw->left_drivetrain_motors.setStopping(vex::brakeType::brake);
     hw ->right_drivetrain_motors.setStopping(vex::brakeType::brake);
     hw->left_intake_expansion_motor.setStopping(vex::brakeType::hold);
@@ -18,31 +18,20 @@ void AutoDrive::drive() {
     // Test code
     std::vector<std::pair<double, double>> path;
     std::pair<double, double> curr_position = rc->starting_pos;
-    std::pair<double, double> targ_position = {-12.0, 12.0};
-    drive_to_position({36,0});
-    // pg->generate_path(path, curr_position, targ_position);
-    path.push_back(curr_position);
-    path.push_back(targ_position);
-    // rotate_to_heading(180.0);
-    // for (int i = 0; i < path.size() - 1; ++i) {
-    //     std::cout << "Before move" << '\n';
-    //     std::cout << "Path: (" << path.at(i).first << ", " << path.at(i).second << ") Next: ("<< path.at(i+1).first << ", " << path.at(i+1).second << ")" << std::endl;
-    //     std::cout << "Odemetry: (" << tm->get_current_position().first << ", " << tm->get_current_position().second << ") " << tm->get_current_heading() << std::endl;
-    //     rotate_and_drive_to_position(path.at(i+1), false);
-    //     std::cout << "After move" << '\n';
-    //     std::cout << "Path: (" << path.at(i+1).first << ", " << path.at(i+1).second << std::endl;
-    //     std::cout << "Odemetry: (" << tm->get_current_position().first << ", " << tm->get_current_position().second << ") " << tm->get_current_heading() << std::endl;
-    //     vex::wait(30, vex::timeUnits::msec);
-    // }
-    // while (true) {
-    // std::cout << "Odemetry: (" << tm->get_current_position().first << ", " << tm->get_current_position().second << ") " << tm->get_current_heading() << std::endl;
-    // hw->controller.Screen.clearScreen();
-    // hw->controller.Screen.setCursor(1,1);
-    // hw->controller.Screen.print("%.1lf, %.1lf", tm->get_current_position().first, tm->get_current_position().second);
-    // hw->controller.Screen.setCursor(2,1);
-    // hw->controller.Screen.print("%.1lf deg", tm->get_current_heading());
-    
-    // }
+    std::pair<double, double> targ_position = {-36.0, -58.0};
+    pg->generate_path(path, curr_position, targ_position);
+
+    for (int i = 0; i < path.size() - 1; ++i) {
+        std::cout << "Before move" << '\n';
+        std::cout << "Path: (" << path.at(i).first << ", " << path.at(i).second << ") Next: ("<< path.at(i+1).first << ", " << path.at(i+1).second << ")" << std::endl;
+        std::cout << "Odemetry: (" << tm->get_current_position().first << ", " << tm->get_current_position().second << ") " << tm->get_current_heading() << std::endl;
+        rotate_and_drive_to_position(path.at(i+1), false);
+        std::cout << "After move" << '\n';
+        std::cout << "Path: (" << path.at(i+1).first << ", " << path.at(i+1).second << std::endl;
+        std::cout << "Odemetry: (" << tm->get_current_position().first << ", " << tm->get_current_position().second << ") " << tm->get_current_heading() << std::endl;
+        vex::wait(30, vex::timeUnits::msec);
+    }
+
 }
 
 void AutoDrive::rotate_to_heading(double heading)
@@ -86,7 +75,7 @@ void AutoDrive::rotate_to_heading(double heading)
     // while (angle_to_travel > 1 && (previous_angle_to_travel - angle_to_travel) >= -0.05) { //TODO: Something isn't working here
     while (fabs(tm->get_current_heading() - heading) > 1.0) { //! REMOVE OR REFACTOR
         // Speeds up as leaving initial angle and slows down as approaching destination
-        std::cout << "      Curr heading: " << tm->get_current_heading() << std::endl;
+        //std::cout << "      Curr heading: " << tm->get_current_heading() << std::endl;
         if (angle_to_travel > total_angle_to_travel/2) {
             // First half of distance
             velocity = atan(fabs(total_angle_to_travel - angle_to_travel)) * 2 
@@ -163,8 +152,8 @@ void AutoDrive::drive_to_position(std::pair<double, double> position, bool ISBAC
     // (change in distance starts majorly increasing instead of decreasing)
     while (fabs(current_distance) > 0.5 && (previous_distance - current_distance) >= -0.01) {
         // Speeds up as leaving initial position and slows down as approaching destination
-        std::cout << "      Current Distance: " << current_distance << " Current Heading: " << tm->get_current_heading() << std::endl;
-        std::cout << "      Odemetry: (" << tm->get_current_position().first << ", " << tm->get_current_position().second << ") " << tm->get_current_heading() << std::endl;
+        //std::cout << "      Current Distance: " << current_distance << " Current Heading: " << tm->get_current_heading() << std::endl;
+        //std::cout << "      Odemetry: (" << tm->get_current_position().first << ", " << tm->get_current_position().second << ") " << tm->get_current_heading() << std::endl;
         if (current_distance >= distance/2) {
             // First half of distance
             velocity = atan(distance - current_distance) * 2 * (max_velocity-min_velocity) / M_PI 
@@ -180,7 +169,7 @@ void AutoDrive::drive_to_position(std::pair<double, double> position, bool ISBAC
         previous_distance = current_distance; // So don't overshoot
         current_distance = tm->get_distance_between_points(tm->get_current_position(), position); 
     }
-        std::cout << "      Current Distance: " << current_distance << " Current Heading: " << tm->get_current_heading() << std::endl;
+        //std::cout << "      Current Distance: " << current_distance << " Current Heading: " << tm->get_current_heading() << std::endl;
 
     hw->drivetrain.stop();  // Stop wheels
     vex::wait(50, vex::timeUnits::msec);  // Wait for odometry wheels to update
