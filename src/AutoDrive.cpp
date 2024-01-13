63645b08fcd3c324df6058bf9c64c5f56ecb040f
Wa Xiong <waxiong77@gmail.com>
2024-01-12 20:49:49 -0900

    New function for offset points

--- src/AutoDrive.cpp
+++ src/AutoDrive.cpp
@@ -198,37 +198,36 @@ void AutoDrive::drive_to_position(std::pair<double, double> position, bool ISBAC
     vex::wait(1000, vex::timeUnits::msec);  // Wait for odometry wheels to update
 }
 
-AutoDrive::get_point_with_offset(GameElement* element)
+std::pair<double, double> AutoDrive::get_point_with_offset(InteractionObject* element)
 {
-    InteractionObject* object = static_cast<InteractionObject*>(element);
     std::pair<double, double> new_position = element->get_position();
 
-    if (object->get_interaction_angle == tm->get_current_heading())
+    if (*(element->get_interaction_angle()) == tm->get_current_heading())
     {
         return new_position;
     }
     
-    // adjusted for heading_adjust, original interaction_angle = ...
+    // adjusted for heading_adjust, original get_interaction_angle = ...
     // ZERO
-    if (object->interaction_angle == 90) {
-        new_position.second - rc->DRIVETRAIN_RADIUS;
+    if (*(element->get_interaction_angle()) == 90) {
+        new_position.second -= rc->DRIVETRAIN_RADIUS;
     }
     // FORTY_FIVE
-    else if (object->interaction_angle == 135) {
-        new_position.first + rc->DRIVETRAIN_RADIUS / sqrt(2);
-        new_position.second - rc->DRIVETRAIN_RADIUS / sqrt(2);
+    else if (*(element->get_interaction_angle()) == 135) {
+        new_position.first += rc->DRIVETRAIN_RADIUS / sqrt(2);
+        new_position.second -= rc->DRIVETRAIN_RADIUS / sqrt(2);
     }
     // NINETY
-    else if (object->interaction_angle == 180) {
-        new_position.first + rc->DRIVETRAIN_RADIUS;
+    else if (*(element->get_interaction_angle()) == 180) {
+        new_position.first += rc->DRIVETRAIN_RADIUS;
     }
     // ONE_THIRTY_FIVE
-    else if (object->interaction_angle == 225) {
-        new_positon.first + rc->DRIVETRAIN_RADIUS / sqrt(2);
-        new_position.second + rc->DRIVETRAIN_RADIUS / sqrt(2);
+    else if (*(element->get_interaction_angle()) == 225) {
+        new_position.first += rc->DRIVETRAIN_RADIUS / sqrt(2);
+        new_position.second += rc->DRIVETRAIN_RADIUS / sqrt(2);
     }
     // ONE_EIGHTY
-    //else if (object->interaction_angle == 0)
+    //else if (element->get_interaction_angle == 0)
     // TWO_SEVENTY
     // NEG_FORTY_FIVE
     // NEG_NINETY
