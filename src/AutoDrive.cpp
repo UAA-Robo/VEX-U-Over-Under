3d19fd3bf9053afbaf96139f142837c60792fd34
Dawson-Packer <Dawson_Packer@outlook.com>
2024-01-14 14:10:48 -0900

    Updating robot config

--- src/AutoDrive.cpp
+++ src/AutoDrive.cpp
@@ -41,6 +41,11 @@ void AutoDrive::drive() {
 
 void AutoDrive::execute_skills_plan() {
 
+    drive_to_position(
+        {
+            
+        }
+    )
     if (rc->ROBOT == SCRAT) {
 
         // Launch match loads
