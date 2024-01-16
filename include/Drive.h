a1806aca92d0211471c2f8282851041249d90ad0
Dawson-Packer <Dawson_Packer@outlook.com>
2024-01-15 16:58:21 -0900

    Testing more plow strategy

--- include/Drive.h
+++ include/Drive.h
@@ -81,4 +81,5 @@ protected:
 
     bool START_CATAPULT_LAUNCH = false;
     bool CATAPULT_STOPPED = true;
+    bool SNOWPLOW_OUT = false;
 };
--- src/AutoDrive.cpp
+++ src/AutoDrive.cpp
@@ -71,14 +71,20 @@ void AutoDrive::execute_skills_plan() {
 
 void AutoDrive::drive_along_path() {
     for (int i = 0; i < path.size() - 1; ++i) {
+        std::cout << "Before move" << '\n';
+        std::cout << "Path: (" << path.at(i).first << ", " << path.at(i).second << ") Next: ("<< path.at(i+1).first << ", " << path.at(i+1).second << ")" << std::endl;
+        std::cout << "Odemetry: (" << tm->get_current_position().first << ", " << tm->get_current_position().second << ") " << tm->get_current_heading() << std::endl;
         rotate_and_drive_to_position(path.at(i+1), false);
-        vex::wait(30, vex::timeUnits::msec);
+        std::cout << "After move" << '\n';
+        std::cout << "Path: (" << path.at(i+1).first << ", " << path.at(i+1).second << std::endl;
+        std::cout << "Odemetry: (" << tm->get_current_position().first << ", " << tm->get_current_position().second << ") " << tm->get_current_heading() << std::endl;
+        vex::wait(50, vex::timeUnits::msec);
     }
 }
 
 void AutoDrive::rotate_to_heading(double heading)
 {
-    std::cout << "Rotating to " << heading << std::endl;
+    // std::cout << "Rotating to " << heading << std::endl;
     // Corrects heading to be from 0-360 from the x axis counterclockwise if applicable
     heading = fmod(heading, 360);
     if (heading < 0) heading += 360;
@@ -102,22 +108,22 @@ void AutoDrive::rotate_to_heading(double heading)
         min_velocity = 20;
         max_velocity = 25;
     }
-    std::cout << "Angle to travel: " << angle_to_travel << std::endl;
+    // std::cout << "Angle to travel: " << angle_to_travel << std::endl;
 
     // Turn wheels opposite of each other
     hw->left_drivetrain_motors.spin(vex::directionType::fwd, -min_velocity * turn_direction, 
         vex::velocityUnits::pct);
     hw->right_drivetrain_motors.spin(vex::directionType::fwd, min_velocity * turn_direction, 
     vex::velocityUnits::pct);
-    std::cout << "Left velocity: " << hw->left_drivetrain_motors.velocity(vex::velocityUnits::pct) << " Right velocity: " << hw->right_drivetrain_motors.velocity(vex::velocityUnits::pct) << std::endl;
+    // std::cout << "Left velocity: " << hw->left_drivetrain_motors.velocity(vex::velocityUnits::pct) << " Right velocity: " << hw->right_drivetrain_motors.velocity(vex::velocityUnits::pct) << std::endl;
     vex::wait(30, vex::timeUnits::msec);    
 
     // Turn until within 1 degrees of desired heading or until it overshoots
     // (change in angle starts majorly increasing instead of decreasing)
     while (angle_to_travel > 1 && (previous_angle_to_travel - angle_to_travel) >= -0.05) { //TODO: Something isn't working here
     // while (fabs(tm->get_current_heading() - heading) > 1.0) { //! REMOVE OR REFACTOR
-        std::cout << "    V: " << velocity << ", Angle to Travel: " << angle_to_travel << std::endl;
-        std::cout << "    Odemetry: (" << tm->get_current_position().first << ", " << tm->get_current_position().second << ") " << tm->get_current_heading() << std::endl;
+        // std::cout << "    V: " << velocity << ", Angle to Travel: " << angle_to_travel << std::endl;
+        // std::cout << "    Odemetry: (" << tm->get_current_position().first << ", " << tm->get_current_position().second << ") " << tm->get_current_heading() << std::endl;
         // Speeds up as leaving initial angle and slows down as approaching destination
         if (angle_to_travel > total_angle_to_travel/2) {
             // First half of distance
@@ -140,8 +146,8 @@ void AutoDrive::rotate_to_heading(double heading)
         if (angle_to_travel > 180) angle_to_travel = 360 - angle_to_travel; 
 
     }
-        std::cout << "    V: " << velocity << ", Angle to Travel: " << angle_to_travel << std::endl;
-        std::cout << "    Odemetry: (" << tm->get_current_position().first << ", " << tm->get_current_position().second << ") " << tm->get_current_heading() << std::endl;
+        // std::cout << "    V: " << velocity << ", Angle to Travel: " << angle_to_travel << std::endl;
+        // std::cout << "    Odemetry: (" << tm->get_current_position().first << ", " << tm->get_current_position().second << ") " << tm->get_current_heading() << std::endl;
 
 
     hw->drivetrain.stop();                  
@@ -153,7 +159,7 @@ void AutoDrive::rotate_to_position(std::pair<double, double> final_position, boo
 {
     // if (IS_USING_GPS_POSITION) tm->set_current_heading(tm->getGPSPosition());
     double heading = tm->get_heading_between_points(tm->get_current_position(), final_position); // Gets absolute angle
-    std::cout << "Intended Heading: " << heading << '\n';
+    // std::cout << "Intended Heading: " << heading << '\n';
     if (ISBACKROTATION)
         heading -= 180;
     rotate_to_heading(heading); 
@@ -212,10 +218,10 @@ void AutoDrive::drive_to_position(std::pair<double, double> position, bool ISBAC
     double drive_direction = ISBACKTOPOSITION ? -1: 1;
     hw->drivetrain.spin(vex::directionType::fwd, min_velocity * drive_direction, 
         vex::velocityUnits::pct);
-    std::cout << "Left velocity: " << hw->left_drivetrain_motors.velocity(vex::velocityUnits::pct) << " Right velocity: " << hw->right_drivetrain_motors.velocity(vex::velocityUnits::pct) << std::endl;
+    // std::cout << "Left velocity: " << hw->left_drivetrain_motors.velocity(vex::velocityUnits::pct) << " Right velocity: " << hw->right_drivetrain_motors.velocity(vex::velocityUnits::pct) << std::endl;
     vex::wait(30, vex::timeUnits::msec);
     
-    std::cout << "Driving " << distance << " inches to (" << position.first << ", " << position.second << ")" << std::endl;
+    // std::cout << "Driving " << distance << " inches to (" << position.first << ", " << position.second << ")" << std::endl;
 
     // Turn until within 0.5 inches of desired distance or until it overshoots 
     // (change in distance starts majorly increasing instead of decreasing)
@@ -240,8 +246,8 @@ void AutoDrive::drive_to_position(std::pair<double, double> position, bool ISBAC
         current_distance = tm->get_distance_between_points(tm->get_current_position(), position); 
     }
         //std::cout << "      Current Distance: " << current_distance << " Current Heading: " << tm->get_current_heading() << std::endl;
-    std::cout << "    Odemetry: (" << tm->get_current_position().first << ", " << tm->get_current_position().second << ") " << tm->get_current_heading() << std::endl;
-    std::cout << "    V: " << velocity << " Current Distance: " << current_distance << std::endl;
+    // std::cout << "    Odemetry: (" << tm->get_current_position().first << ", " << tm->get_current_position().second << ") " << tm->get_current_heading() << std::endl;
+    // std::cout << "    V: " << velocity << " Current Distance: " << current_distance << std::endl;
 
     hw->drivetrain.stop();  // Stop wheels
     vex::wait(1000, vex::timeUnits::msec);  // Wait for odometry wheels to update
@@ -292,103 +298,109 @@ void AutoDrive::run_plow_strategy() {
     std::pair<double, double> prep_pos;
     std::pair<double, double> target_pos;
 
+    if (!SNOWPLOW_OUT) snowplow_out();
+
+
     // Ram at top of red goal
-    snowplow_out();
     target_pos = mp->goals[3]->get_position();
-    prep_pos = target_pos;
-    prep_pos.second += 18.0; // Offset from goal by 1.5 feet
+    prep_pos.first = 70.02 - 14.0;
+    prep_pos.second = target_pos.second + 21.0; // Offset from goal by almost 2 feet
     target_pos.second += rc->DRIVETRAIN_RADIUS;
     pg->generate_path(this->path, tm->get_current_position(), prep_pos);
     drive_along_path();
     this->rotate_to_heading(-90.0);
     drive_to_position(target_pos, false);
     vex::wait(500, vex::timeUnits::msec);
-    snowplow_in();
+    // snowplow_in();
     drive_to_position(prep_pos, true);
 
     // Ram at side-top of red goal
-    snowplow_out();
+    // snowplow_out();
+    std::cout << "1" << '\n';
     target_pos = mp->goals[0]->get_position();
     prep_pos = target_pos;
-    prep_pos.first += 18.0;
+    std::cout << "2" << '\n';
+    prep_pos.first += 21.0;
     target_pos.first += rc->DRIVETRAIN_RADIUS;
+    std::cout << "3" << '\n';
     pg->generate_path(this->path, tm->get_current_position(), prep_pos);
     drive_along_path();
     this->rotate_to_heading(0.0);
     drive_to_position(target_pos, false);
     vex::wait(500, vex::timeUnits::msec);
-    snowplow_in();
+    // snowplow_in();
     drive_to_position(prep_pos, true);
 
     // Ram at side-bottom of red goal
-    snowplow_out();
+    // snowplow_out();
     target_pos = mp->goals[2]->get_position();
     prep_pos = target_pos;
-    prep_pos.first += 18.0;
+    prep_pos.first += 21.0;
     target_pos.first += rc->DRIVETRAIN_RADIUS;
     pg->generate_path(this->path, tm->get_current_position(), prep_pos);
     drive_along_path();
     this->rotate_to_heading(0.0);
     drive_to_position(target_pos, false);
     vex::wait(500, vex::timeUnits::msec);
-    snowplow_in();
+    // snowplow_in();
     drive_to_position(prep_pos, true);
 
     // Ram at bottom of red goal
-    snowplow_out();
+    // snowplow_out();
     target_pos = mp->goals[4]->get_position();
-    prep_pos = target_pos;
-    prep_pos.second -= 18.0; // Offset from goal by 1.5 feet
+    prep_pos.first = 70.02 - 14.0;
+    prep_pos.second = target_pos.second - 21.0; // Offset from goal by almost 2 feet
     target_pos.second -= rc->DRIVETRAIN_RADIUS;
     pg->generate_path(this->path, tm->get_current_position(), prep_pos);
     drive_along_path();
     rotate_to_heading(90.0);
     drive_to_position(target_pos, false);
     vex::wait(500, vex::timeUnits::msec);
-    snowplow_in();
+    // snowplow_in();
     drive_to_position(prep_pos, true);
 
     // Returning direction -------------------------------------------------------------------------
 
     // Ram at side-bottom of red goal
-    snowplow_out();
+    // snowplow_out();
     target_pos = mp->goals[2]->get_position();
     prep_pos = target_pos;
-    prep_pos.first += 18.0;
+    prep_pos.first += 21.0;
     target_pos.first += rc->DRIVETRAIN_RADIUS;
     pg->generate_path(this->path, tm->get_current_position(), prep_pos);
     drive_along_path();
     rotate_to_heading(0.0);
     drive_to_position(target_pos, false);
     vex::wait(500, vex::timeUnits::msec);
-    snowplow_in();
+    // snowplow_in();
     drive_to_position(prep_pos, true);
 
     // Ram at side-top of red goal
-    snowplow_out();
+    // snowplow_out();
     target_pos = mp->goals[0]->get_position();
     prep_pos = target_pos;
-    prep_pos.first += 18.0;
+    prep_pos.first += 21.0;
     target_pos.first += rc->DRIVETRAIN_RADIUS;
     pg->generate_path(this->path, tm->get_current_position(), prep_pos);
     drive_along_path();
     rotate_to_heading(0.0);
     drive_to_position(target_pos, false);
     vex::wait(500, vex::timeUnits::msec);
-    snowplow_in();
+    // snowplow_in();
     drive_to_position(prep_pos, true);
 
+    //? May be covered already
     // Ram at top of red goal
-    snowplow_out();
-    target_pos = mp->goals[3]->get_position();
-    prep_pos = target_pos;
-    prep_pos.second += 18.0; // Offset from goal by 1.5 feet
-    target_pos.second += rc->DRIVETRAIN_RADIUS;
-    pg->generate_path(this->path, tm->get_current_position(), prep_pos);
-    drive_along_path();
-    rotate_to_heading(-90.0);
-    drive_to_position(target_pos, false);
-    vex::wait(500, vex::timeUnits::msec);
-    snowplow_in();
-    drive_to_position(prep_pos, true);
+    //// snowplow_out();
+    // target_pos = mp->goals[3]->get_position();
+    // prep_pos = target_pos;
+    // prep_pos.second += 18.0; // Offset from goal by 1.5 feet
+    // target_pos.second += rc->DRIVETRAIN_RADIUS;
+    // pg->generate_path(this->path, tm->get_current_position(), prep_pos);
+    // drive_along_path();
+    // rotate_to_heading(-90.0);
+    // drive_to_position(target_pos, false);
+    // vex::wait(500, vex::timeUnits::msec);
+    //// snowplow_in();
+    // drive_to_position(prep_pos, true);
 }
