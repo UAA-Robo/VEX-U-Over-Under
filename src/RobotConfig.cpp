063e6803b059dc887db7f2647c2118d5f7f4212d
Dawson-Packer <Dawson_Packer@outlook.com>
2024-02-19 22:53:36 -0900

    Late night edits

--- src/RobotConfig.cpp
+++ src/RobotConfig.cpp
@@ -19,6 +19,9 @@ RobotConfig::RobotConfig(Hardware* hardware)
         hw->left_odometry.setReversed(false);
         hw->back_odometry.setReversed(true);
 
+        hw->right_intake_motor.setReversed(true);
+        hw->left_intake_motor.setReversed(false);
+
         /*
         ----SET DIMENSIONS (in) --------------------------------------------------------------
         */
